3038fe750882795981bfaee4fc27695b80172041
playders <ephram@lahode.ch>
2021-04-03 19:18:37 +0200

    Premier commit

---
+++ js_client/main.js
@@ -0,0 +1,127 @@
+//configuration du jeu
+var config = {
+    type : Phaser.AUTO,
+    width : 800,
+    height : 600,
+    scene : {
+        preload : preload,
+        create : create,
+        update : update,
+    },
+    physics :{
+        default : "arcade",
+        arcade : {
+            gravity : {y : 1000}
+        }
+    }
+}
+//variable et constant
+var players = null;
+var cursor = null;
+var Akey;
+var Skey;
+var laserReady = true;
+var laser = false
+var laserReady1 = true;
+var laser1 = false
+
+const game = new Phaser.Game(config);
+
+//charger les image
+
+function preload(){
+    this.load.image("tiles", "tiled1.png");
+    this.load.tilemapTiledJSON("map","../json/tiled.json");
+    this.load.image("Players","coeur.png");
+    this.load.image("back","backgrond.png");
+    this.load.image("alien","alienYellow.png");
+    this.load.image("alien1","alienYellow_walk1.png");
+    this.load.image("alien2","alienYellow_walk2.png");
+    //charger audio
+    this.load.audio("kick","kick.ogg");
+}
+//camera,placement des image
+function create(){
+
+    this.tilemap = this.make.tilemap({key: "map"});
+    this.tileset = this.tilemap.addTilesetImage("maman","tiles");
+
+    this.downLayer = this.tilemap.createLayer("bot",this.tileset,0,0);
+    this.worldLayer = this.tilemap.createLayer("world",this.tileset,0,0);
+    this.topLayer = this.tilemap.createLayer("top",this.tileset,0,0);
+
+
+    var cameraCentreX = this.cameras.main.centerX;
+    var cameraCentreY = this.cameras.main.centerY;
+    this.add.sprite(cameraCentreX,cameraCentreY,"back");
+    players = this.physics.add.sprite(cameraCentreX,cameraCentreY,"Players");
+    players.setScale(0.5);
+
+    //animation alien
+    this.anims.create({
+        key: "alienAnim",
+        frames: [
+            {key: "alien1"},
+            {key: "alien2"}
+        ],
+        frameRate: 8,
+        repeat: -1
+    });
+    var alienYellow = this.add.sprite(500,cameraCentreY,"alien1").play("alienAnim");
+    var tween = this.tweens.add({
+        targets : alienYellow,
+        x : 800,
+        ease : "Linear",
+        duration : 1000,
+        yoyo : true,
+        repeat : -1,
+        onStart : function (){},
+        onComplete : function (){},
+        onYoyo : function (){ alienYellow.flipX = !alienYellow.flipX},
+        onRepeat : function (){alienYellow.flipX = !alienYellow.flipX}
+    });
+    //bouton du clavier
+    cursor = this.input.keyboard.createCursorKeys();
+    Akey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    Skey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+}
+
+function update(time, delta){
+    //deplacement du player
+    if (cursor.left.isDown){
+        players.x = players.x -= 7;
+    }
+
+    if (cursor.right.isDown){
+        players.x = players.x += 7;
+    }
+
+    //lancer laser
+    if (laser && laserReady){
+        this.sound.play("kick");
+        laserReady = false;
+    }
+    
+    if (laser1 && laserReady1){
+        this.sound.play("kick");
+        laserReady1 = false;
+    }
+    
+    if (Akey.isDown && laserReady) {
+        laser = true;
+    }
+    
+    if (Akey.isUp){
+        laser = false;
+        laserReady = true;
+    }
+    
+    if (Skey.isDown && laserReady){
+        laser1 = true;
+    }
+    
+    if (Skey.isUp){
+        laser1 = false;
+        laserReady1 = true;
+    }
+}
---
+++ serveur.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+var gestionPage = require("./gestionPage");
+
+const PORT = "8080";
+
+var serveur =http.createServer(traitReq);
+serveur.listen(PORT);
+
+function traitReq(requete, reponse){
+    var monObj = url.parse(requete.url);
+
+    if (monObj.pathname === "/"){
+         monObj.pathname = "/index.html";
+    }
+
+
+    if(monObj.pathname !== "/favicon.ico"){
+        var dataPreparation = gestionPage.preparerlesDonnees(monObj);
+        var data = {};
+        data.contentType = dataPreparation.contentType;
+        data.pageHtml = fs.readFileSync(dataPreparation.dossier + dataPreparation.fichier,dataPreparation.encodage);
+        gestionPage.evoyerlesDonnees(reponse,data)
+    }
+
+}
